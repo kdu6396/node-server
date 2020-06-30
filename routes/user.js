09b0f697c2349ec64b19198c7bcf04edeabdac84
kdu6396 <34638523+kdu6396@users.noreply.github.com>
2020-06-30 17:45:38 +0900

    메일 인증 기능 추가
    메일 인증 기능 추가

--- routes/user.js
+++ routes/user.js
@@ -5,5 +5,7 @@ const userController = require('../controllers/userController')
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
+router.post('/mailcheck',userController.mailcheck);
+router.post('/mailtokencheck',userController.mailtokencheck);
 
 module.exports = router;
